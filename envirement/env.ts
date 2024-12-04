08f5f36f54079d83d3690bf601ead84f28abd560
vindexTOS <giorgikutateladze1998@gmail.com>
2024-12-04 16:46:37 +0400

    change end point

--- envirement/env.ts
+++ envirement/env.ts
@@ -1,6 +1,5 @@
 export const envirement = {
     // baseUrl: "http://192.168.1.111:8000/api/",
-    baseUrl: "http://3.71.18.216/api/",
+    baseUrl: "https://lift.eideas.io/api/",
     apiKey: "z7#D4k9@A9",
   };
-  
